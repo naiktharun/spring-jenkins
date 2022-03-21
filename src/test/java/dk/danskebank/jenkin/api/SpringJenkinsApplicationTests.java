31bb5c2562380527c9ed04138a402ebb83dd43a7
tharun1234 <naiktharun@gmail.com>
2022-03-21 20:19:08 +0530

    Checking pass scenario email

--- src/test/java/dk/danskebank/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/dk/danskebank/jenkin/api/SpringJenkinsApplicationTests.java
@@ -16,7 +16,7 @@ class SpringJenkinsApplicationTests {
 	private final int expected = 5;
 
 	@Test
-	= contextLoads() {
+	void contextLoads() {
 		
 		logger.info("Test Case executing..");
 		assertEquals(actual, expected);
