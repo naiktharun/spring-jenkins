33ee90c4c4ac1d75608f7137907d77bb47e2dad5
tharun1234 <naiktharun@gmail.com>
2022-03-21 18:37:33 +0530

    Changes in the test

--- src/test/java/dk/danskebank/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/dk/danskebank/jenkin/api/SpringJenkinsApplicationTests.java
@@ -1,26 +1,29 @@
 package dk.danskebank.jenkin.api;
 
-import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.slf4j.*;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 	
 	
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	private final Integer ACTUAL = 9;
+	private final Integer EXPECTED = 9;
 
 	@Test
 	void contextLoads() {
 		
 		logger.info("Test Case executing..");
-		assertEquals(true, true);
+	
+
 		
 	}
+
+
 
 }
